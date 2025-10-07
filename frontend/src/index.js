d16efda17aca5ebc5354ede1ca0b2e439a8260b0
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-10-07 19:17:04 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Import Leaflet's CSS for map rendering
 import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
