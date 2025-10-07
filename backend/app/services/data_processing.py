6d130bec8076b89b4dfa7996be0090d9687023fb
Rajeev <147076514+Rajeev-08@users.noreply.github.com>
2025-10-07 19:23:32 +0530

    Update data_processing.py

--- backend/app/services/data_processing.py
+++ backend/app/services/data_processing.py
@@ -1,4 +1,3 @@
-# backend/app/services/data_processing.py
 
 import pandas as pd
 
@@ -12,18 +11,12 @@ def load_and_preprocess_data(file_stream):
     # Data Preprocessing
     df['TIME OCC'] = df['TIME OCC'].astype(str).str.zfill(4)
 
-    # THE FIX: Added the `format` parameter to pd.to_datetime.
-    # This makes parsing much faster and eliminates the UserWarning.
-    # We are parsing the date from 'DATE OCC' and then adding the specific time from 'TIME OCC'.
-    # Note: We parse the date part and let the original logic add the correct time.
-    # For a column like '01/08/2020 12:00:00 AM', this is robust.
+  
     df['datetime_occ'] = pd.to_datetime(df['DATE OCC'], errors='coerce') + \
                          pd.to_timedelta(df['TIME OCC'].str[:2].astype(int), unit='h') + \
                          pd.to_timedelta(df['TIME OCC'].str[2:].astype(int), unit='m')
     
-    # We can be even more explicit if the date column format is always the same.
-    # For a format like '01/08/2020 12:00:00 AM', the following is faster, though the above logic is also correct.
-    # To keep the original logic of combining date and time separately:
+ 
     just_date = pd.to_datetime(df['DATE OCC']).dt.date
     df['datetime_occ'] = pd.to_datetime(just_date) + \
                          pd.to_timedelta(df['TIME OCC'].str[:2].astype(int), unit='h') + \
